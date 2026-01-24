3006ee0bf6d7b4899dc015be896aa3edd20a3744
paulius <danpaulius@gmail.com>
2026-01-24 13:00:29 +0200

    Current Lithuanian dataset version

--- dataset/lithuanian_dataset.py
+++ dataset/lithuanian_dataset.py
@@ -18,6 +18,24 @@ except ImportError:
     from utils import split_data_file, process_splits
 
 
+INPUT_FILES_RACE = [
+    ('DATASET No. 1 Ethnicity _ nationality _ race_HS.v1.csv', 1),
+    ('DATASET No. 1 Ethnicity _ nationality _ race_N.v1.csv', 0),
+]
+INPUT_FILES_ORIENTATION = [
+    ('DATASET No. 2 S. Orientation _ gender_HS.v1.csv', 1),
+    ('DATASET No. 2 S. Orientation _ gender_N.v1.csv', 0)
+]
+INPUT_FILES_COUNTRIES = [
+    ('DATASET No. 3 Countries_HS.v1.csv', 1),
+    ('DATASET No. 3 Countries_N.v1.csv', 0)
+]
+INPUT_FILES_POLITICAL = [
+    ('DATASET No. 4 Political_viewa_N.v1.csv', 0),
+    ('DATASET No. 4 Political_views_HS.v1.csv', 1)
+]
+
+
 def preprocess_targets(final_df: pd.DataFrame):
     # Normalize name and set target to Other when it is not available
     final_df['target'] = final_df['target'].str.replace(' ', '-')
@@ -29,17 +47,19 @@ def preprocess_targets(final_df: pd.DataFrame):
     return final_df
 
 
-def _create_long_format_dataset_hate(file_path, output_path, dataset_name="lith_dataset", additional_data: str=None, 
-                                    create_false_entries=False, balance_dataset=False, sample_ratio=1, clear_dirs=True):
+def _create_long_format_dataset_hate(
+        data_path, data_inputs, output_path, dataset_name="lith_dataset", additional_data: str=None, 
+        create_false_entries=False, balance_dataset=False, sample_ratio=1, clear_dirs=True
+):
     if isinstance(output_path, str):
         output_path = Path(output_path)
+    if isinstance(data_path, str):
+        data_path = Path(data_path)
     if clear_dirs:
         shutil.rmtree(output_path/dataset_name, ignore_errors=True)
         shutil.rmtree(output_path/f"{dataset_name}_multi", ignore_errors=True)
-    final_df = pd.concat([
-        pd.read_excel(file_path, sheet_name='Hate', header=0, usecols=['Comment', 'Target']).assign(Value=1),
-        pd.read_excel(file_path, sheet_name='Neutralūs', header=0, usecols=['Comment', 'Target']).assign(Value=0)
-    ])
+    read_file = lambda x: pd.read_csv(data_path/x[0], sep=';', header=0, usecols=['Comment', 'Target']).assign(Value=x[1])
+    final_df = pd.concat(list(map(read_file, data_inputs)))
     final_df.columns = ['text', 'target', 'value']
     if additional_data is not None:
         add_df = pd.read_csv(additional_data)
@@ -56,18 +76,30 @@ def _create_long_format_dataset_hate(file_path, output_path, dataset_name="lith_
     process_splits(final_df, output_path, dataset_name, balance_dataset=balance_dataset, sample_ratio=sample_ratio)
 
 
-def _create_long_format_dataset_levels(file_path, output_path, dataset_name="lith_dataset", 
-                                      balance_dataset=False, sample_ratio=1, clear_dirs=True):
+def _create_long_format_dataset_levels(
+        data_path, data_inputs, output_path, dataset_name="lith_dataset", 
+        balance_dataset=False, sample_ratio=1, clear_dirs=True
+):
     if isinstance(output_path, str):
         output_path = Path(output_path)
+    if isinstance(data_path, str):
+        data_path = Path(data_path)
     if clear_dirs:
         shutil.rmtree(output_path/dataset_name, ignore_errors=True)
         shutil.rmtree(output_path/f"{dataset_name}_multi", ignore_errors=True)
-    levels_df = pd.concat([
-        pd.read_excel(file_path, sheet_name='Hate', header=0, usecols=['Comment', 'Target', 'HS Level Derived'])\
-            .rename(columns={'HS Level Derived': 'value'}),
-        pd.read_excel(file_path, sheet_name='Neutralūs', header=0, usecols=['Comment', 'Target']).assign(value="Level 0")
-    ])
+
+    def read_file_levels(input):
+        usecols = ['Comment', 'Target']
+        if input[1] == 1:
+            usecols.append('HS Level')
+        data = pd.read_csv(data_path/input[0], sep=';', header=0, usecols=usecols)
+        if input[1] == 0:
+            data = data.assign(value='Level 0')
+        elif input[1] == 1:
+            data = data.rename(columns={'HS Level': 'value'})
+        return data
+        
+    levels_df = pd.concat(list(map(read_file_levels, data_inputs)))
     levels_df.columns = ['text', 'target', 'value']
     levels_df = preprocess_targets(levels_df)
     levels_df['target'] = levels_df['target'].apply(lambda x: x + "-Level")
@@ -75,15 +107,19 @@ def _create_long_format_dataset_levels(file_path, output_path, dataset_name="lit
     process_splits(levels_df, output_path, dataset_name, balance_dataset=balance_dataset, sample_ratio=sample_ratio)
 
 
-def create_long_format_dataset(file_path, output_path, dataset_name="lith_dataset", additional_data: str=None, 
-                               create_false_entries=False, balance_dataset=False, sample_ratio=1, 
-                               add_levels_data=False):
+def create_long_format_dataset(
+        file_path, data_inputs, output_path, dataset_name="lith_dataset", 
+        additional_data: str=None, create_false_entries=False, balance_dataset=False, 
+        sample_ratio=1, add_levels_data=False
+):
     _create_long_format_dataset_hate(
-        file_path, output_path, dataset_name, additional_data, create_false_entries, balance_dataset, sample_ratio, clear_dirs=True
+        file_path, data_inputs, output_path, dataset_name, additional_data, 
+        create_false_entries, balance_dataset, sample_ratio, clear_dirs=True
     )
     if add_levels_data:
         _create_long_format_dataset_levels(
-            file_path, output_path, dataset_name=dataset_name, balance_dataset=False, sample_ratio=sample_ratio, clear_dirs=False
+            file_path, data_inputs, output_path, dataset_name=dataset_name, 
+            balance_dataset=False, sample_ratio=sample_ratio, clear_dirs=False
         )
 
 
@@ -111,26 +147,44 @@ def create_wide_format_dataset(file_path, output_path, fill_value=0):
 
 
 if __name__ == '__main__':
-    file_path = Path("../raw")/'DATASET No. 1 Ethnicity _ nationality _ race.xlsx'
+    data_path = Path("../data")
     output_path = 'data'
-    create_long_format_dataset(file_path, output_path)
-    create_long_format_dataset(file_path, output_path, 
-        additional_data=Path("../augmentation")/'lthate'/'generated.csv', 
-        dataset_name="lith_dataset_gen"
-    )
-    create_long_format_dataset(file_path, output_path, 
-        additional_data=None, 
-        dataset_name="lith_dataset_balanced",
-        create_false_entries=True,
-        balance_dataset=True,
-        sample_ratio=1.5
-    )
-    create_long_format_dataset(file_path, output_path, 
-        additional_data=None, 
-        dataset_name="lith_dataset_levels",
-        create_false_entries=True,
-        balance_dataset=True,
-        sample_ratio=1.5,
-        add_levels_data=True
-    )
-    create_wide_format_dataset(file_path, output_path)
+    # create_long_format_dataset(file_path, output_path, 
+    #     additional_data=Path("../augmentation")/'lthate'/'generated.csv', 
+    #     dataset_name="lith_dataset_gen"
+    # )
+    NAMES_MAP = [
+        (INPUT_FILES_RACE, 'race'),
+        (INPUT_FILES_ORIENTATION, 'orientation'),
+        (INPUT_FILES_COUNTRIES, 'countries'),
+        (INPUT_FILES_POLITICAL, 'political')
+    ]
+    for data_inputs, name in NAMES_MAP:
+        create_long_format_dataset(
+            data_path, 
+            data_inputs=data_inputs, 
+            output_path=output_path,
+            dataset_name=f"lith_dataset_{name}"
+        )
+        create_long_format_dataset(
+            data_path, 
+            data_inputs=data_inputs, 
+            output_path=output_path, 
+            additional_data=None, 
+            dataset_name=f"lith_dataset_balanced_{name}",
+            create_false_entries=True,
+            balance_dataset=True,
+            sample_ratio=1.5
+        )
+        create_long_format_dataset(
+            data_path, 
+            data_inputs=data_inputs, 
+            output_path=output_path, 
+            additional_data=None, 
+            dataset_name=f"lith_dataset_levels_{name}",
+            create_false_entries=True,
+            balance_dataset=True,
+            sample_ratio=1.5,
+            add_levels_data=True
+        )
+    # create_wide_format_dataset(data_path, output_path)
